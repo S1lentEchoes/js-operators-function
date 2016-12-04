1eaf175fa4267f250f84c5efe8ef8695414ee41c
S1lentEchoes <s1lent3choes@gmail.com>
2016-12-03 23:01:17 -0600

    Added JS Files
    Added the actual function files.

---
+++ operators.js
@@ -0,0 +1,18 @@
+/*----------------------------------------------------------------------*\
+-------------------- #OPERATOR-FUNCTIONS -------------------
+\*----------------------------------------------------------------------*/
+
+/* *
+ * Simple operator functions in case any calculations are need for
+ * addition, subtraction, multiplication, division, or modulo
+ */
+
+var operators = {
+
+    '+': function(a, b) { return a + b; },
+    '-': function(a, b) { return a - b; },
+    '*': function(a, b) { return a * b; },
+    '/': function(a, b) { return a / b; },
+    '%': function(a, b) { return a % b; }
+
+};
